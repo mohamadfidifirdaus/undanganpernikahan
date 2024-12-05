7c280299fa0f856ea7ec2bd4ba945f5650c4f649
mohamadfidifirdaus <65572730+mohamadfidifirdaus@users.noreply.github.com>
2024-12-05 13:25:08 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,72 @@
+<?php
+
+$url = "http://localhost/undanganonline";
+$title="Undangan Pernikahan";
+$subtitle="Aldi &amp; Herlina";
+$countdown = "2024-12-29 09:00:00";
+$google_map_embed_link = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.4733159434068!2d107.46948440000001!3d-6.587937500000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e690fe1e6d6ff3f%3A0xe36a978497b9624d!2sCF69%2BRQH%2C%20Pasawahankidul%2C%20Kec.%20Pasawahan%2C%20Kabupaten%20Purwakarta%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1733326084950!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+// $backsound_music = 'https://www.chosic.com/wp-content/uploads/2021/11/Beautiful-Piano.mp3';
+
+$images = [
+    'popup'=>"images/aldi1.jpg",
+    'bride'=>"images/bride.jpg",
+    'groom'=>"images/groom.jpg",
+];
+
+$hero = [
+    'image'=>'images/img_bg_1.jpg',
+    'title'=>"Aldi &amp; Herlina ",
+    'desc'=>"Menuju Pernikahan yang Sakinah Mawaddah Warahmah",
+    'link_url'=>'https://calendar.google.com/calendar/',
+    'link_button'=>'Buat Pengingat'
+];
+
+$bride = [
+    'name'=>"Herlina Widyastuti",
+    'desc'=>"Putri ketiga dari Bpk. Ujang supriatna dan Ibu. Jarsih",
+];
+$groom = [
+    'name'=>"Muhammad Aldi",
+    'desc'=>"Putra ketiga dari Bpk. Sapril dan Ibu. Priyatni",
+];
+
+$event1 = [
+    'title'=>'Akad Nikah',
+    'time'=>'09.00-11.00',
+    'date'=>'Minggu, <br> 29 Desember 2024',
+    'place'=>'Gang Depan Bank BRI, Jl. Kapten Halim, Pasawahan, Kec. Pasawahan, Kabupaten Purwakarta, Jawa Barat 41172',
+];
+$event2 = [
+    'title'=>'Resepsi',
+    'time'=>'11.00-selesai',
+    'date'=>'Minggu, <br> 29 Desember 2024',
+    'place'=>'Gang Depan Bank BRI, Jl. Kapten Halim, Pasawahan, Kec. Pasawahan, Kabupaten Purwakarta, Jawa Barat 41172',
+];
+$timelines = [
+    [
+        'image'=>'images/story1.jpg',
+        'title'=>'2019',
+        'date'=>'',
+        'desc'=>'Kami bertemu pertama kali di grup chat pekerjaan pada tahun 2019. Karena berada di satu kerjaan yang sama, kami seringkali mengobrol dan bercanda bersama. Namun, kami tidak menyadari jika ada perasaan sayang.'
+    ],
+    [
+        'image'=>'images/story2.jpg',
+        'title'=>'2022',
+        'date'=>'',
+        'desc'=>'Juni tahun 2022 mulai menjalin hubungan,dengan niatan dan tujuan yg sama akhirnya kita memutuskan untuk melanjutkan ke jenjang yang lebih serius,yakni bulan April 2024 kami bertunangan, sekaligus menentukan pernikahan kami yakni pada bulan Desember tahun 2024'
+    ],
+    [
+        'image'=>'images/story3.jpg',
+        'title'=>'2024',
+        'date'=>'',
+        'desc'=>'Perasaan cinta yang kuat membuat kami yakin menuju jenjang pernikahan. Untuk menjalani hari-hari bersama dan berbahagia dalam jalinan rumah tangga.'
+    ],
+];
+$galleries = [
+    'images/foto1.jpg',
+    'images/foto2.jpg',
+    'images/foto3.jpg',
+    'images/foto4.jpg',
+    'images/foto5.jpg',
+    'images/foto6.jpg',
+];
